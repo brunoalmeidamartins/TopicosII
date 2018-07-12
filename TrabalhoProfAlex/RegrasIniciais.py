fd1bca2a3706f1e60325f75c941e1e33d1a3cf48
BrunoMartins <sgtbrunoalmeida@gmail.com>
2018-07-12 13:46:42 -0300

    12_07_2018

--- TrabalhoProfAlex/RegrasIniciais.py
+++ TrabalhoProfAlex/RegrasIniciais.py
@@ -47,6 +47,8 @@ def executaComandosOVS(comando):
 '''
 Define regras de saida dos hosts nos switchs
 '''
+
+'''
 listaConexoes = []
 
 for i in conexoes:
@@ -55,7 +57,11 @@ for i in listaConexoes:
     comando = 'ovs-ofctl add-flow ' + conexoes[i][0] + ' dl_dst=' + host[i] + ',actions=output:' +str(conexoes[i][1])
     executaComandosOVS(comando)
 '''
+
+'''
 Define as outras regras de saida
+'''
+
 '''
 listaSwitches = []
 listaHosts = []
@@ -86,4 +92,46 @@ for i in listaSwitches:
                     executaComandosOVS(comando)
                 else:
                     comando = 'ovs-ofctl add-flow ' + i + ' dl_dst='+ host[j]+',actions=output:14'
+                    executaComandosOVS(comando)
+
+
+'''
+
+listaConexoes = []
+
+for i in conexoes:
+    listaConexoes.append(i)
+for i in listaConexoes:
+    comando = 'ovs-ofctl add-flow ' + conexoes[i][0] + ' dl_dst=' + host[i] +',nw_proto=1'+ ',actions=output:' +str(conexoes[i][1])
+    executaComandosOVS(comando)
+
+listaSwitches = []
+listaHosts = []
+for i in switch:
+    listaSwitches.append(i)
+for i in host:
+    listaHosts.append(i)
+
+for i in listaSwitches:
+    #print('Switch '+i)
+    if(len(linkPrincipal[i]) == 1):
+        for j in listaConexoes:
+            if(conexoes[j][0] != i): #Somente para fora do switch
+                comando = 'ovs-ofctl add-flow ' + i + ' dl_dst='+ host[j]+',nw_proto=1'+',actions=output:'+str(linkPrincipal[i][0])
+                executaComandosOVS(comando)
+    else:
+        for j in listaConexoes:
+            if(conexoes[j][0] != i): #Somente para fora do switch
+                #Verificar para qual suite ele vai
+                lista = host[j].split(':')
+                lista = lista[4]
+                switch_alvo = 's'+lista[1]
+                if(switch_alvo == 's1'):
+                    comando = 'ovs-ofctl add-flow ' + i + ' dl_dst='+ host[j]+',nw_proto=1'+',actions=output:11'
+                    executaComandosOVS(comando)
+                elif(switch_alvo == 's2'):
+                    comando = 'ovs-ofctl add-flow ' + i + ' dl_dst='+ host[j]+',nw_proto=1'+',actions=output:12'
+                    executaComandosOVS(comando)
+                else:
+                    comando = 'ovs-ofctl add-flow ' + i + ' dl_dst='+ host[j]+',nw_proto=1'+',actions=output:14'
                     executaComandosOVS(comando)
